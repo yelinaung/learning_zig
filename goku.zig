67bbd30ae407aa753634a5c79e7770fcc6dcaae1
Ye Lin Aung <me@yelinaung.com>
2024-04-24 21:53:36 +0800

    include more

--- goku.zig
+++ goku.zig
@@ -2,13 +2,17 @@
 // builtin functions
 // They are provided by compiler, not by the std lib
 const std = @import("std");
-const User = @import("model/user.zig").User;
+const user = @import("model/user.zig");
+const User = user.User;
+const MAX_POWER = user.MAX_POWER;
+
 // if main is not marked as 'pub'
 // you get "error: 'main' is not marked 'pub'"
 pub fn main() !void {
-    const user = User{
+    const local_user = User{
         .power = 9001,
         .name = "Goku",
     };
-    std.debug.print("{s}'s power is {d}\n", .{ user.name, user.power });
+    std.debug.print("{s}'s power is {d}\n", .{ local_user.name, local_user.power });
+    std.debug.print("Max power is {d}\n", .{MAX_POWER});
 }
--- model/user.zig
+++ model/user.zig
@@ -2,3 +2,5 @@ pub const User = struct {
     power: u64,
     name: []const u8,
 };
+
+pub const MAX_POWER = 100_100;
